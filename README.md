cd4f85f6ba7e062816f38a19f54f60bf83bfb391
nana950819 <luckyman100530@gmail.com>
2019-01-11 18:56:09 +0800

    Initial working POS implementation

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Health-BlockChain
+# Blockchain
+Blockchain using Golang.
+
+### Keys
+
+The Blockchain uses ECDSA (224 bits) keys. 
+When a user first joins the blockchain a random key will be generated.
+
+Keys are encoded using base58.
+
+Given x, y as the components of the public key, the key is generated as following:
+
+```
+	base58(BigInt(append(x as bytes, y as bytes)))
+```
+
+### Proof of Stake
+In order to sign a transaction and send it to the validator, proof of stake is required. 
+
+### Protocol
+
+The blockchain runs on port `9750` and uses TCP to handle connections among peers.
+
+##### Message
+
---
+++ scripts/setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+printf "Getting github.com/joho/godotenv"
+go get github.com/joho/godotenv
+
+printf "Getting
+go get github.com/ipfs/go-log
+printf "Getting go get github.com/libp2p/go-libp2p"
+go get github.com/libp2p/go-libp2p
+printf "Getting go get github.com/libp2p/go-libp2p-crypto"
+go get github.com/libp2p/go-libp2p-crypto
+printf "Getting go get github.com/libp2p/go-libp2p-host"
+go get github.com/libp2p/go-libp2p-host
+printf "Getting go get github.com/libp2p/go-libp2p-net"
+go get github.com/libp2p/go-libp2p-net
+printf "Getting go get github.com/libp2p/go-libp2p-peer"
+go get github.com/libp2p/go-libp2p-peer
+printf "Getting go get github.com/libp2p/go-libp2p-peerstore"
+go get github.com/libp2p/go-libp2p-peerstore
+printf "Getting go get github.com/multiformats/go-multiaddr"
+go get github.com/multiformats/go-multiaddr
+printf "Getting go get github.com/whyrusleeping/go-logging"
+go get github.com/whyrusleeping/go-logging
+printf "Complete"
